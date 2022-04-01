6cc81e1ec3d5ecd99252f123fa3447863a53383a
Walter González-Plata Cortes <waltergpc@hotmail.com>
2022-03-31 20:51:55 -0600

    empty app component for new app

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,11 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
-</script>
+<script></script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Hello World</h1>
 </template>
 
 <style>
-@import "./assets/base.css";
+@import './assets/base.css';
 
 #app {
   max-width: 1280px;
